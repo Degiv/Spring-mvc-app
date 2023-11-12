b33dd495f5bc903bd1cc9afb065c6d55b6e6247e
Ivan Degtyarev <degtyarevivano@yandex.ru>
2023-11-12 15:57:56 +0300

    Lesson 22. CRUD REST DAO

---
+++ Lesson15.SpringMVCApp1/src/main/java/com/degiv/springmvcwebapp/dao/PersonDAO.java
@@ -0,0 +1,30 @@
+package com.degiv.springmvcwebapp.dao;
+
+import com.degiv.springmvcwebapp.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//для обучения пока DAO класс работает с ArrayList, который потом будет заменен на БД
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT = 0;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Ivan"));
+        people.add(new Person(++PEOPLE_COUNT, "John"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse((null));
+    }
+}
