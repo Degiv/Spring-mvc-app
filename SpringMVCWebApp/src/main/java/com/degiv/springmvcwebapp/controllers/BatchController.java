3e7167456bbf6d455b42acd2b2ee8529647bbeaf
Ivan Degtyarev <degtyarevivano@yandex.ru>
2023-11-15 22:17:05 +0300

    use SERIAL type for id

--- SpringMVCWebApp/src/main/java/com/degiv/springmvcwebapp/controllers/BatchController.java
+++ SpringMVCWebApp/src/main/java/com/degiv/springmvcwebapp/controllers/BatchController.java
@@ -1,6 +1,7 @@
-package com.degiv.springmvcwebapp.config;
+package com.degiv.springmvcwebapp.controllers;
 
 import com.degiv.springmvcwebapp.dao.PersonDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/test-batch-update")
 public class BatchController {
     private final PersonDAO personDAO;
+
+    @Autowired
+    public BatchController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
     @GetMapping()
     public String index() {
         return "batch/index";
@@ -16,6 +23,13 @@ public class BatchController {
 
     @GetMapping("/without")
     public String withoutBatch() {
+        personDAO.testMultipleUpdate();
+        return "redirect:/people";
+    }
 
+    @GetMapping("/with")
+    public String withBatch() {
+        personDAO.testBatchUpdate();
+        return "redirect:/people";
     }
 }
